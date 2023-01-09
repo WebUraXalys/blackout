4c0874e2be857a265452dcf8a180d49a1cca6876
Dmytro Ivanets <ivanecdima63@gmail.com>
2023-01-09 14:05:45 +0200

    change admin panel and models

--- blackout/parserapp/admin.py
+++ blackout/parserapp/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from parserapp.models import Street, Building, Region
 
-admin.site.register(Street)
-admin.site.register(Building)
-admin.site.register(Region)
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ['pk','name', 'city', 'OTG',"region"]
+    ordering = ['pk']
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ['pk','address', 'street', 'group']
+
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['pk','name']
+    ordering = ['pk']
 
--- blackout/parserapp/models.py
+++ blackout/parserapp/models.py
@@ -1,18 +1,39 @@
 from django.db import models
 
 class Region(models.Model):
-    name = models.CharField(max_length=40,unique=True)
+    name = models.CharField(verbose_name="Назва", max_length=40,unique=True,blank=True)
+
+    class Meta():
+        verbose_name = "Область"
+        verbose_name_plural = "Області"
+    def __str__(self):
+        return self.name
 
 
 class Street(models.Model):
-    name = models.CharField(max_length=40)
-    city = models.CharField(max_length=40)
-    OTG = models.CharField(max_length=40)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="Назва",max_length=40,blank=True)
+    city = models.CharField(verbose_name="Місто", max_length=40,blank=True)
+    OTG = models.CharField(verbose_name="ОТГ",max_length=40,blank=True)
+    region = models.ForeignKey(Region, verbose_name="Регіон",blank=True, on_delete=models.CASCADE)
+
+    class Meta():
+        verbose_name = "Вулиця"
+        verbose_name_plural = "Вулиці"
+
+    def __str__(self):
+        return self.name
 
 
 class Building(models.Model):
-    address = models.CharField(max_length=40)
-    street = models.ForeignKey(Street,on_delete=models.PROTECT)
-    group = models.IntegerField(default=1)
+    address = models.CharField(verbose_name="Адреса",max_length=40,blank=True)
+    street = models.ForeignKey(Street,verbose_name="Вулиця",blank=True, on_delete=models.PROTECT)
+    group = models.IntegerField(verbose_name="Група",default=None, blank=True)
+
+    class Meta():
+        verbose_name = "Будинок"
+        verbose_name_plural = "Будинки"
+
+    def __str__(self):
+        return self.address
+    
 
