b2b7b4d6f30045b18971115bf5f275692f10c632
Viktor Vilson <viktorgeniphone@gmail.com>
2023-01-13 11:09:41 +0200

    updated view, new item

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuItem = (props) => {
     return (
         <a href="#" className="menu-item">
-            <img src={props.item.img} alt=""/>
+            <img src={props.item.img} alt="picture"/>
             <span>{props.item.title}</span>
         </a>
     );
