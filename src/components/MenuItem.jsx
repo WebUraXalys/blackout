cda351e9ff3d264a5eafa4274027e8606f780427
Viktor Vilson <viktorgeniphone@gmail.com>
2023-01-16 22:17:01 +0200

    updated frontend

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const MenuItem = (props) => {
     return (
-        <a href="#" className="menu-item">
-            <img src={props.item.img} alt="picture"/>
-            <span>{props.item.title}</span>
-        </a>
+        <div className="menu_el">
+            <img className="svg_menu" src={props.item.img} alt="picture"/>
+            <p className="menu_text">{props.item.title}</p>
+        </div>
     );
 };
 
