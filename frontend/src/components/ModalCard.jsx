8719a615b3c34ce345ed40adb6288f805e2b0bf3
Viktor Vilson <viktorgeniphone@gmail.com>
2023-02-03 12:59:12 +0200

    Validation v2.0.0

--- frontend/src/components/ModalCard.jsx
+++ frontend/src/components/ModalCard.jsx
@@ -1,36 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import {TextField} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+
 
 const ModalCard = () => {
-
     const [open, setOpen] = React.useState(false);
+    const [myFocus, setMyFocus] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [state, setState] = React.useState({
+
+    const [values, setValues] = useState({
         city: "",
         street: "",
-        number: ""
-    })
+        number: "",
+    });
+    const inputs = [
+        {
+            id: 1,
+            name: "city",
+            margin: "dense",
+            variant: "outlined",
+            size: "small",
+            label: "City",
+            pattern: "^[A-Za-z0-9]{3,14$}",
+            type: "text",
+            required: true,
+            error: myFocus === true,
+            helperText: "Write correct city(ex: Lviv)"
+        },
+        {
+            id: 2,
+            name: "street",
+            margin: "dense",
+            variant: "outlined",
+            size: "small",
+            label: "Street",
+            pattern: "^[A-Za-z0-9]{2,16}$",
+            type: "text",
+            required: true,
+            error: myFocus === true,
+            helperText: "Write correct street(ex: Skisna)"
+        },
+        {
+            id: 3,
+            name: "number",
+            margin: "dense",
+            variant: "outlined",
+            size: "small",
+            label: "Number",
+            pattern: "^[A-Za-z0-9]{3,10}$",
+            type: "text",
+            required: true,
+            error: myFocus === true,
+            helperText: "Write correct number(ex: 126)"
+        }
+    ]
 
-    const handleChange = (e) => {
-        const value = e.target.value;
-        setState({
-            ...state,
-            [e.target.name]: value
-        });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+    const handleFocus = (e) => {
+        setMyFocus(true)
+        console.log(e.target)
     }
 
-    const sendData = () => {
-        setState({
-            city: "",
-            street: "",
-            number: ''
+    const onChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
         })
     }
+
 
     const style = {
         input: { color: 'white'},
@@ -53,6 +97,7 @@ const ModalCard = () => {
         },
     }
 
+    console.log(values)
     return (
         <Box sx={{display: 'flex', justifyContent: 'center'}}>
             <Button sx={{margin: '10px 0', color: '#ffd60a',
@@ -67,73 +112,43 @@ const ModalCard = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <form>
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            display: 'flex', justifyContent: 'center', flexDirection: 'column',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            width: 300,
-                            borderRadius: '10px',
-                            p: 2,
-                            backdropFilter: 'blur(16px) saturate(181%)',
-                            "::webkitBackdropFilter": 'blur(16px) saturate(181%)',
-                            backgroundColor: 'rgba(17, 25, 40, 0.96)',
-                            border: '1px solid rgba(255, 255, 255, 0.125)',
-                        }}
-                    >
-                        <TextField sx={style}
-                            id="City"
-                            label="City"
-                            name="city"
-                            helperText=" "
-                            variant="outlined"
-                            size="small"
-                            type="text"
-                            margin="dense"
-                            onChange={handleChange}
-                            value={state.city}
-                        />
-                        <TextField sx={style}
-                            id="Street"
-                            label="Street"
-                            name="street"
-                            helperText=" "
-                            variant="outlined"
-                            size="small"
-                            type="text"
-                            margin="dense"
-                            onChange={handleChange}
-                            value={state.street}
-
-                        />
-                        <TextField sx={style}
-                            id="House number"
-                            label="Number"
-                            name="number"
-                            helperText=" "
-                            variant="outlined"
-                            size="small"
-                            type="text"
-                            margin="dense"
-                            onChange={handleChange}
-                            value={state.number}
-
-                        />
-                        <Button
-                            type="submit"
-                            sx={{margin: '20px 0 10px'}}
-                            variant="outlined"
-                            endIcon={<SendIcon />}
-                            onClick={sendData}
-                        >
-                            Send
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    backdropFilter: 'blur(16px) saturate(180%)',
+                    webkitBackdropFilter: 'blur(16px) saturate(180%)',
+                    backgroundColor: 'rgba(17, 25, 40, 0.82)',
+                    borderRadius: '12px',
+                    border: '1px solid rgba(255, 255, 255, 0.125)',
+                    p: 4
+                }}>
+                    <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+                        {inputs.map((input) => <TextField
+                            sx={style}
+                            helperText={input.helperText || " "}
+                            error={input.error}
+                            key={input.id}
+                            name={input.name}
+                            margin={input.margin}
+                            variant={input.variant}
+                            size={input.size}
+                            type={input.type}
+                            label={input.label}
+                            pattern={input.pattern}
+                            required={input.required}
+                            value={values[input.name]}
+                            onChange={onChange}
+                            onBlur={handleFocus}
+                        />)}
+                        <Button type="submit" sx={{marginTop: 2}} size="small" variant="outlined" endIcon={<SendIcon />}>
+                            Submit
                         </Button>
-                    </Box>
+                    </form>
 
-                </form>
+                </Box>
             </Modal>
         </Box>
     );
