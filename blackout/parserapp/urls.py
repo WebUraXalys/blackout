364cb1416f85120a51a0529389397f42691659a7
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-03-05 17:19:12 +0200

    List of cities, streets, buildings. Functionality for deleting city or street

--- blackout/parserapp/urls.py
+++ blackout/parserapp/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("", start, name="start")]
+urlpatterns = [
+    path("", start, name="start"),
+    path("<city>", city_view, name="city"),
+    path("predelete/<city>", predelete_view, name="delete"),
+    path("predelete/<city>/<street>", predelete_view, name="delete-street"),
+    path("delete/<city>", delete_view, name="confirm-delete-city"),
+    path("delete/<city>/<street>", delete_view, name="confirm-delete-street"),
+    path("<city>/<street>", streets_view, name="street")
+]
--- blackout/parserapp/views.py
+++ blackout/parserapp/views.py
@@ -1,9 +1,52 @@
-from .services.parser import start_browser, get_page
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import Streets, Buildings
 
 
+@staff_member_required()
 def start(request):
-    pass
+    cities = Streets.objects.order_by('City').values('City').distinct()
+    context = {'cities': cities}
+    return render(request, 'parserapp/city.html', context=context)
+
+
+@staff_member_required()
+def city_view(request, city):
+    streets = Streets.objects.filter(City=city).order_by('Name')
+    context = {'city': city,
+               'streets': streets}
+    return render(request, 'parserapp/streets.html', context=context)
+
+
+@staff_member_required()
+def predelete_view(request, **kwargs):
+    """
+    Method for confirming deleting objects. Kwargs gets city and street
+    """
+    try:
+        street = kwargs['street']
+    except:
+        street = None
+    context = {'city': kwargs['city'],
+               'street': street}
+
+    return render(request, 'parserapp/delete/predelete.html', context=context)
+
+
+@staff_member_required()
+def delete_view(request, **kwargs):
+    try:
+        Streets.objects.get(City=kwargs['city'], Name=kwargs['street']).delete()
+    except:
+        Streets.objects.filter(City=kwargs['city']).delete()
+    return redirect('/parser/')
+
+
+@staff_member_required()
+def streets_view(request, street, city):
+    street_obj = Streets.objects.get(Name=street, City=city)
+    buildings = Buildings.objects.filter(Street=street_obj)
+    context = {'city': city,
+               'street': street,
+               'buildings': buildings}
+    return render(request, 'parserapp/buildings.html', context=context)
