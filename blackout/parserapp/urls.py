d94fc1209b5b94c110e6ff2d73f8e016cf839fef
Oleksandr Molibozhko <molibdecode@gmail.com>
2023-04-19 16:00:55 +0300

    Revert "Error Parser_Test"
    This reverts commit b072235dc58811f00cec1ec656291ab4e42d34b6.

--- blackout/parserapp/urls.py
+++ blackout/parserapp/urls.py
@@ -2,4 +2,3 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path("", start, name="start")]
-
--- blackout/parserapp/views.py
+++ blackout/parserapp/views.py
@@ -1,24 +1,9 @@
-#from .services.parser import start_browser, get_page
+from .services.parser import start_browser, get_page
 
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Streets
+from django.http import HttpResponse
+
 
 def start(request):
-    data = [
-        {'name': 'Arhypenka', 'city': 'Lviv', 'otg': 'Lvivska', 'region': 'Lvivskyi'},
-        {'name': 'Tarnavskogo', 'city': 'Lviv', 'otg': 'Lvivska', 'region': 'Lvivskyi'},
-        {'name': 'Stryiska', 'city': 'Lviv', 'otg': 'Lvivska', 'region': 'Lvivskyi'},
-    ]
-
-    for item in data:
-        street = Streets.objects.create(
-            Name=item['name'], City=item['city'], OTG=item['otg'], Region=item['region']
-        )
-
-        print(street.Name, street.City, street.OTG, street.Region)
-
-    return render(request, 'templates/data_list.html', {'data': data})
-
-#start(request=HttpResponse)
+    pass
