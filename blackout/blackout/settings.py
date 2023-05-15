829dadc965887e272b9d3893969cd5b9f085d3a0
Kyrylo Boiko <119179727+Ferkkkk@users.noreply.github.com>
2023-05-15 15:42:40 +0300

    Update settings.py
    Updated "SECRET_KEY"

--- blackout/blackout/settings.py
+++ blackout/blackout/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a'
+SECRET_KEY = [os.getenv('SERVER_URL'), '127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
