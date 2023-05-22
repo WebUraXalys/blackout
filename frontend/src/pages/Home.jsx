c48bada7da97fda55bacf68da9af28e2979a6a05
WebVladPasichnyk <vladislav.pasichnyk@gmail.com>
2023-05-22 16:07:57 +0300

    Home + merge

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,53 +1,87 @@
 import React, { useState } from 'react';
+import LocationList from "../components/LocationList.jsx";
+import LocationForm from "../components/LocationForm.jsx";
 
 function Home() {
-const [showModal, setShowModal] = useState(false);
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
-const [email, setEmail] = useState('');
 
-const handleConfirm = () => {
-	// create a new component with the input data
-	const newComponent = <NewComponent firstName={firstName} lastName={lastName} email={email} />;
-	// do something with the new component
-	console.log(newComponent);
-	// close the modal
-	setShowModal(false);
-}
+		const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    }
 
-return (
-	<div>
-		<button onClick={() => setShowModal(true)}>Open Modal</button>
-		{showModal && (
-			<div className="modal">
-				<div className="modal-content">
-					<h2>Enter your information</h2>
-					<label htmlFor="first-name">First Name:</label>
-					<input type="text" id="first-name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-					<label htmlFor="last-name">Last Name:</label>
-					<input type="text" id="last-name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-					<label htmlFor="email">Email:</label>
-					<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-					<div className="button-group">
-						<button onClick={() => setShowModal(false)}>Cancel</button>
-						<button onClick={handleConfirm}>Confirm</button>
-					</div>
-				</div>
-			</div>
-		)}
-	</div>
-);
-}
+	    const [locations, setLocations] = useState([
+        {id: 1, icon: 'home', title: "Home", electricity: false}
+    ])
 
-function NewComponent({ firstName, lastName, email }) {
-	return (
-		<div>
-			<h2>New Component</h2>
-			<p>First Name: {firstName}</p>
-			<p>Last Name: {lastName}</p>
-			<p>Email: {email}</p>
+	     return (
+			<div className="div main_container_home">
+				<div>
+            <LocationList locations={locations}/>
+       	 </div>
 		</div>
-	);
+    );
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const [showModal, setShowModal] = useState(false);
+// const [firstName, setFirstName] = useState('');
+// const [lastName, setLastName] = useState('');
+// const [email, setEmail] = useState('');
+
+// const handleConfirm = () => {
+// 	// create a new component with the input data
+// 	const newComponent = <NewComponent firstName={firstName} lastName={lastName} email={email} />;
+// 	// do something with the new component
+// 	console.log(newComponent);
+// 	// close the modal
+// 	setShowModal(false);
+// }
+
+// return (
+// 	<div>
+// 		<button onClick={() => setShowModal(true)}>Open Modal</button>
+// 		{showModal && (
+// 			<div className="modal">
+// 				<div className="modal-content">
+// 					<h2>Enter your information</h2>
+// 					<label htmlFor="first-name">First Name:</label>
+// 					<input type="text" id="first-name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+// 					<label htmlFor="last-name">Last Name:</label>
+// 					<input type="text" id="last-name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+// 					<label htmlFor="email">Email:</label>
+// 					<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+// 					<div className="button-group">
+// 						<button onClick={() => setShowModal(false)}>Cancel</button>
+// 						<button onClick={handleConfirm}>Confirm</button>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		)}
+// 	</div>
+// );
+// }
+
+// function NewComponent({ firstName, lastName, email }) {
+// 	return (
+// 		<div>
+// 			<h2>New Component</h2>
+// 			<p>First Name: {firstName}</p>
+// 			<p>Last Name: {lastName}</p>
+// 			<p>Email: {email}</p>
+// 		</div>
+// 	);
 }
 
 export default Home;
--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,34 +1,60 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
+// import LocationList from "../components/LocationList.jsx";
+// import LocationForm from "../components/LocationForm.jsx";
+
+// const Timer = () => {
+
+//     const [open, setOpen] = useState(false);
+//     const handleOpen = () => {
+//         setOpen(true);
+//     }
+
+//     const [locations, setLocations] = useState([
+//         {id: 1, icon: 'home', title: "Home", electricity: false},
+//         {id: 2, icon: 'work', title: "Work", electricity: true},
+//     ])
+
+//     const createLocation = (newLocation) => {
+//         setLocations([...locations, newLocation])
+//     }
+
+
+
+//     return (
+//         <div>
+//             {open && (
+//                 <LocationForm setOpen={setOpen} create={createLocation} setLocations={setLocations}/>
+//             )}
+
+//             <LocationList locations={locations} handleOpen={handleOpen}/>
+//         </div>
+//     );
+// };
+
+// export default Timer;
+import React, { useState } from 'react';
 import LocationList from "../components/LocationList.jsx";
 import LocationForm from "../components/LocationForm.jsx";
 
 const Timer = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  }
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    }
+  const [locations, setLocations] = useState([
+    { id: 1, icon: 'home', title: "Home", electricity: false },
+    { id: 2, icon: 'work', title: "Work", electricity: true },
+  ])
 
-    const [locations, setLocations] = useState([
-        {id: 1, icon: 'home', title: "Home", electricity: false},
-        {id: 2, icon: 'work', title: "Work", electricity: true},
-    ])
-
-    const createLocation = (newLocation) => {
-        setLocations([...locations, newLocation])
-    }
-
-
-
-    return (
-        <div>
-            {open && (
-                <LocationForm setOpen={setOpen} create={createLocation} setLocations={setLocations}/>
-            )}
-
-            <LocationList locations={locations} handleOpen={handleOpen}/>
-        </div>
-    );
+  return (
+    <div>
+      {open && (
+        <LocationForm setOpen={setOpen} setLocations={setLocations} />
+      )}
+      <LocationList locations={locations} handleOpen={handleOpen} />
+    </div>
+  );
 };
 
 export default Timer;
